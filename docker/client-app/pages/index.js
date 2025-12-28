3434d705866ff576108993dad654578f1c9de31c
diptishegar <diptishegar6@gmail.com>
2025-12-28 19:06:30 +0530

    ci-run

--- docker/client-app/pages/index.js
+++ docker/client-app/pages/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default function Home() {
   const [value, updateValue] = useState("create new data");
   const [input, setInput] = useState("");
-  const baseUrl = `http://localhost:5001`;
+  const baseUrl = `http://${process.env.REACT_APP_API_URL}:5001`;
   const getDataUrl = `${baseUrl}/data`;
   const createDataUrl = `${baseUrl}/create`;
 
