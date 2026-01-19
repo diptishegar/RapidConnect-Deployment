68d6ba2db7885de5b759db9045aa8a37b332bf55
diptishegar <diptishegar6@gmail.com>
2026-01-19 12:36:29 +0530

    ci-run frontend updated

--- docker/client-app/pages/index.js
+++ docker/client-app/pages/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default function Home() {
   const [value, updateValue] = useState("create new data");
   const [input, setInput] = useState("");
-  const baseUrl = "/api";
+  const baseUrl = `/api`;
   const getDataUrl = `${baseUrl}/data`;
   const createDataUrl = `${baseUrl}/create`;
 
